9c8e58086a5f1d11e0cc21c4c9eafc00948e07a0
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2024-02-06 13:55:33 -0600

    Tasks responsive layout

--- src/components/tasks/index.jsx
+++ src/components/tasks/index.jsx
@@ -1,46 +1,55 @@
-import { useEffect , useState} from "react";
-import {getTasks} from "../../api";
+import { useEffect, useState } from "react";
+import { getTasks } from "../../api";
 import Task from './show';
 import Button from 'react-bootstrap/Button';
 import StatusDropDown from "../status-dropdown";
 import CreateTask from "./create";
-const Tasks=()=> {
+import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+const Tasks = () => {
     const [createModalShow, setCreateModalShow] = useState(false);
-    const [tasks, setTasks]=useState([]);
-    const onStatusChange=(status)=>{
+    const [tasks, setTasks] = useState([]);
+    const onStatusChange = (status) => {
         getItems(status);
-        console.log('Clicked'+status);
+        console.log('Clicked' + status);
     }
-    const getItems=(status=null)=>{
-        if(status){
-            getTasks(status).then((data)=>{
+    const getItems = (status = null) => {
+        if (status) {
+            getTasks(status).then((data) => {
                 setTasks(data.data);
             });
             return;
         }
-        getTasks().then((data)=>{
+        getTasks().then((data) => {
             console.log(data.data);
             setTasks(data.data);
             return;
         });
     }
-    useEffect(()=>{
+    useEffect(() => {
         getItems();
-    },[]);
+    }, []);
     return <>
-    <div className="h1">
-    Tasks
-    </div>
-    <StatusDropDown onClickHandler={onStatusChange}/>
-        <Button variant="primary" onClick={() => setCreateModalShow(true)}>
-        Add Task
-      </Button>
-
-      <CreateTask
-        show={createModalShow}
-        onHide={() => setCreateModalShow(false)}
-      />
-    {tasks.map((task, index)=><Task key={index} id={index} name={task.name} description={task.description} status={task.status} dueDate={task.dueDate}/>)}
+        <div className="h1">
+            Tasks
+        </div>
+        <Stack direction="horizontal" gap={1} className="py-2">
+            <div className="ms-auto"><Button variant="primary" onClick={() => setCreateModalShow(true)}>
+                Add Task
+            </Button></div>
+            <div><StatusDropDown onClickHandler={onStatusChange} /></div>
+        </Stack>
+        <CreateTask
+            show={createModalShow}
+            onHide={() => setCreateModalShow(false)}
+        />
+        <Container fluid className="py-3">
+            <Row>
+                {tasks.map((task, index) => <Col xs={12} md={3}><Task key={index} id={index} name={task.name} description={task.description} status={task.status} dueDate={task.dueDate} /></Col>)}
+            </Row>
+        </Container>
     </>
 }
 export default Tasks;
